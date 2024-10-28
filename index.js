da3ff8e081b7be705bda1fc325ed3aa4d603bef4
Mohammed Zoabi <zhamodi@gmail.com>
2024-10-28 15:57:22 +0200

    הגדרות חיבור לתקיית SQL

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+// הגדרות חיבור למסד הנתונים
+//הגדרות אלה משתנים ממשתמש לאחר 
+const config = {
+    user: 'DESKTOP-SBH3ON3/Hamodi', // שנה לשם המשתמש שלך
+    password: '', // שנה לסיסמה שלך
+    server: 'DESKTOP-SBH3ON3\SQLEXPRESS01', // כתובת השרת (אם מסד הנתונים מקומי)
+    database: 'SecurityCompany',
+    options: {
+      encrypt: true,
+      trustServerCertificate: true
+    }
+  };
+  
